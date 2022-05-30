6db075f3d2cb7c0f5028fb3aba2ac81fa85ae731
Hamzenis Kryeziu <hamzenis@gmail.com>
2022-05-30 23:56:03 +0200

    Update prog5.1c.asm

--- prog5.1c.asm
+++ prog5.1c.asm
@@ -1,59 +1,67 @@
 .text
-
-	#jump zur main:
-	j main
+	
+main:
+	addiu $4, $0, 210
+	addiu $5, $0, 28
+	jal func
+	addiu $4, $0, 25
+	addiu $5, $0, 35
+	jal func
+	addiu $4, $0, 49
+	addiu $5, $0, 42
+	jal func
+	addiu $4, $0, 17
+	addiu $5, $0, 3
+	jal func
+	addiu $4, $0, 17
+	addiu $5, $0, 51
+	jal func
+	
+ende:
+	j ende	
 	
 func:
 	#Zeile 3
 	add $2, $0, $5
 
 	#Zeile 4
-	beq $4, $0, ende
+	beq $4, $0, loop
 	
 	#Zeile 5
+z5:
 	beq $5, $0, z9
 	
 	#Zeile6
+z6a:
 	slt $27, $5, $4
-	bne $27, $0, z6
-	sll $0, $0, 0	#No Op
+	bne $27, $0, z6b
 	
+
 	#Zeile7
-	beq $27, $0, z7
-	sll $0, $0, 0	#No Op
+z7a:
+	beq $27, $0, z7b
 	
 	#Zeile8
-	or $26, $0, $0		#Jump Back
-	lui $26, 0x0040		#    |
-	ori $26, $26, 0xc	#    |
-	jr $26			#Jump Back
-	
+z8:
+	j z5			#Jump Back
+
 	
 #Zeile6
-z6:
+z6b:
 	subu $4, $4, $5
-	or $26, $0, $0		#Jump Back
-	lui $26, 0x0040		#    |
-	ori $26, $26, 0x18	#    |
-	jr $26			#Jump Back
+	j z7a			#Jump Back
 	
 #Zeile7
-z7:
+z7b:
 	subu $5, $5, $4
-	or $26, $0, $0		#Jump Back
-	lui $26, 0x0040		#    |
-	ori $26, $26, 0x20	#    |
-	jr $26			#Jump Back
+	j z8			#Jump Back
 		
 #Zeile 9
 z9:
 	add $2, $4, $0
 	
-#Zeile 10(ehemaliger Loop)
-ende:
+#Zeile 10
+loop:
 	jr $31
 
-main: 
-	addiu $4, $0, 42
-	addiu $5, $0, 49
-	jal func
+	
