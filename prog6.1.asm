896972fba3e560f77bc282f578ad7f78f2727f7e
Hamzenis Kryeziu <hamzenis@gmail.com>
2022-06-13 15:26:46 +0200

    Update prog6.1.asm

--- prog6.1.asm
+++ prog6.1.asm
@@ -1,17 +1,39 @@
 # void strtolower(char *str)
 
+.data
+
+str: .asciiz "TOLowerCASE"
+
+
 .text
 
 main:
+	la  $a0, str
+	jal strtolower
+	
+	
+end:
+	j end
 
 
 
 strtolower:
+	addiu $8, $0, 0x40	# Grenzen der Großbuchstaben in Ascii : 0x41 = A, 0x5a = Z
+	addiu $9, $0, 0x5b	# siehe oben
+	addiu $10, $a0, 0	# Kopie der Adresse mit dem String
 
-
-
-
-.data
-
-str: .asciiz "TO Lower CASE"
-
+	loop:
+		lbu $11, 0($10)		# Laedt den ersten Buchstaben in Register 12
+		beq $11, $0, jumpback	# Wenn leer dann jumpback
+		slt $12, $11, $9	# Wenn der Buchstabe in $11 kleiner ist als die Grenze der Großbuchstaben Ascii's , die in $9 gespeichert sind
+		slt $13, $8, $11	# Logik wie oben nur anders rum
+		bne $12, $13, loop	# Falls die Grenzen nicht passen, dann nächster Buchstabe
+		addiu $11, $11, 32	# Mit +32 dez wird vom Großbuchstaben in den Bereich der Kleinbuchstaben Ascii's 
+		sb $11, 0($10)
+		addiu $10, $10, 4
+		j loop
+		
+		
+	jumpback:
+		jr $31
+	
