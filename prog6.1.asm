43a479f0e6e3fd7e2f2dbe93912b36e93ed65f3e
Hamzenis Kryeziu <hamzenis@gmail.com>
2022-06-13 15:58:05 +0200

    Update prog6.1.asm

--- prog6.1.asm
+++ prog6.1.asm
@@ -23,14 +23,15 @@ strtolower:
 	addiu $10, $a0, 0	# Kopie der Adresse mit dem String
 
 	loop:
-		lbu $11, 0($10)		# Laedt den ersten Buchstaben in Register 12
+		lbu $11, 0($10)		# Laedt den ersten Buchstaben in Register 11
+		addiu $10, $10, 1
 		beq $11, $0, jumpback	# Wenn leer dann jumpback
 		slt $12, $11, $9	# Wenn der Buchstabe in $11 kleiner ist als die Grenze der Großbuchstaben Ascii's , die in $9 gespeichert sind
 		slt $13, $8, $11	# Logik wie oben nur anders rum
 		bne $12, $13, loop	# Falls die Grenzen nicht passen, dann nächster Buchstabe
-		addiu $11, $11, 32	# Mit +32 dez wird vom Großbuchstaben in den Bereich der Kleinbuchstaben Ascii's 
-		sb $11, 0($10)
-		addiu $10, $10, 1
+		addiu $11, $11, 32	# Mit +32 dez wird vom Großbuchstaben in den Bereich der Kleinbuchstaben Ascii's
+		addiu $20, $10, -1	# Decrement
+		sb $11, 0($20)
 		j loop
 		
 		
