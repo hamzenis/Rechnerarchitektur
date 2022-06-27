de18a97add00e8d448ce6a3ec6529330244f2431
Hamzenis Kryeziu <hamzenis@gmail.com>
2022-06-27 15:35:53 +0200

    Aufgabenblatt7
    added 7.1 & 7.2
    fixes 6.1

--- prog6.1.asm
+++ prog6.1.asm
@@ -42,15 +42,15 @@ strtolower:
 	addiu $10, $a0, 0	# Kopie der Adresse mit dem String
 
 loopToLow:
-    	lbu $11, 0($10)			# Laedt den ersten Buchstaben in Register 11
+    	lbu $11, ($10)			# Laedt den ersten Buchstaben in Register 11
    	addiu $10, $10, 1		# Increment für den nächsten Durchlauf
    	beq $11, $0, jumpbackToLow	# Wenn leer dann jumpback
    	slt $12, $11, $9		# Wenn der Buchstabe in $11 kleiner ist als die Grenze der Großbuchstaben Ascii's , die in $9 gespeichert sind
     	slt $13, $8, $11		# Logik wie oben nur anders rum
     	bne $12, $13, loopToLow		# Falls die Grenzen nicht passen, dann nächster Buchstabe
     	addiu $11, $11, 32		# Mit +32 dez wird vom Großbuchstaben in den Bereich der Kleinbuchstaben Ascii's
-    	addiu $20, $10, -1		# Decrement zum speichern an der richtigen Adresse, aber mit einem anderen Register
-    	sb $11, 0($20)			# Store des Kleinbuchstaben
+    	addiu $14, $10, -1		# Decrement zum speichern an der richtigen Adresse, aber mit einem anderen Register
+    	sb $11, ($14)			# Store des Kleinbuchstaben
     	j loopToLow
     
 jumpbackToLow:
@@ -61,28 +61,28 @@ jumpbackToLow:
 # void strturnaround(char *str)
 # Start TurnAroundFunction
 strturnaround:
-	addiu $a1, $a0, 0 		# Kopie der Startadresse des Strings
-	addiu $a2, $a0, 0		# Wird für die Endadresse verwendet
+	addiu $t6, $a0, 0 		# Kopie der Startadresse des Strings
+	addiu $t7, $a0, 0		# Wird für die Endadresse verwendet
 	
 loopTurnHoch:
-	lb $t0, ($a2)			# Laden des AsciiZeichens
-	beqz $t0, subTurn		# Wenn $a2 gleich 0 ist, dann wird zum eigentlichen Umdrehen gesprungen
-	addiu $a2, $a2, 1		# Es wird hochgezaehlt bis das Ende des Strings erreicht wurde, der mit einer Null terminiert
+	lb $t0, ($t7)			# Laden des AsciiZeichens
+	beqz $t0, subTurn		# Wenn $t7 gleich 0 ist, dann wird zum eigentlichen Umdrehen gesprungen
+	addiu $t7, $t7, 1		# Es wird hochgezaehlt bis das Ende des Strings erreicht wurde, der mit einer Null terminiert
 	j loopTurnHoch
 
 subTurn:
-	addiu $a2, $a2, -1		# Damit das letzte Zeichen erreicht wird, weil der Zeiger auf dem Zeichen danach ist
+	addiu $t7, $t7, -1		# Damit das letzte Zeichen erreicht wird, weil der Zeiger auf dem Zeichen danach ist
 	
 turnAroundloop:
-	bleu $a2, $a1,  jumpbackTurnAr	# Wenn die $a2 und die $a1 sich in der Mitte treffen oder $a2 unter $a1 fällt dann Endet die Funktion
-	lbu $t0, ($a1)			# Laden der Buchstaben in Registern
-	lbu $t1, ($a2)			# s.o.
+	bleu $t7, $t6,  jumpbackTurnAr	# Wenn die $a2 und die $a1 sich in der Mitte treffen oder $a2 unter $a1 fällt dann Endet die Funktion
+	lbu $t0, ($t6)			# Laden der Buchstaben in Registern
+	lbu $t1, ($t7)			# s.o.
 
-	sb $t1, ($a1)			# Einspeichern der Buchstaben an den Registern
-	sb $t0, ($a2)			# s.o.
+	sb $t1, ($t6)			# Einspeichern der Buchstaben an den Registern
+	sb $t0, ($t7)			# s.o.
 	
-	addiu $a1, $a1, 1		# Erhöhen der Adresse
-	addiu $a2, $a2, -1		# Erniedrigen der Adresse
+	addiu $t6, $t6, 1		# Erhöhen der Adresse
+	addiu $t7, $t7, -1		# Erniedrigen der Adresse
 	
 	j turnAroundloop
 	
@@ -96,33 +96,32 @@ jumpbackTurnAr:
 # Start isPalindrom Function
 
 strispalindrom:
-	addiu $a1, $a0, 0 		# Kopie der Startadresse des Strings
-	addiu $a2, $a0, 0		# Wird für die Endadresse verwendet
+	addiu $t6, $a0, 0 		# Kopie der Startadresse des Strings
+	addiu $t7, $a0, 0		# Wird für die Endadresse verwendet
 	
 loopPalinHoch:
-	lb $t0, ($a2)			# Laden des AsciiZeichens
-	beqz $t0, subPalin		# Wenn $a2 gleich 0 ist, dann wird zum eigentlichen Umdrehen gesprungen
-	addiu $a2, $a2, 1		# Es wird hochgezaehlt bis das Ende des Strings erreicht wurde, der mit einer Null terminiert
+	lb $t0, ($t7)			# Laden des AsciiZeichens
+	beqz $t0, subPalin		# Wenn $t7 gleich 0 ist, dann wird zum eigentlichen Umdrehen gesprungen
+	addiu $t7, $t7, 1		# Es wird hochgezaehlt bis das Ende des Strings erreicht wurde, der mit einer Null terminiert
 	j loopPalinHoch
 
 subPalin:
-	addiu $a2, $a2, -1		# Damit das letzte Zeichen erreicht wird, weil der Zeiger auf dem Zeichen danach ist
+	addiu $t7, $t7, -1		# Damit das letzte Zeichen erreicht wird, weil der Zeiger auf dem Zeichen danach ist
 
 palinCheck:
-	bleu $a2, $a1, jumpbackPalin	# Wenn die $a2 und die $a1 sich in der Mitte treffen oder $a2 unter $a1 fällt dann Endet die Funktion
-	lbu $t0, ($a1)			# Laden der Buchstaben in Registern
-	lbu $t1, ($a2)			# s.o.
+	bleu $t7, $t6, jumpbackPalin	# Wenn die $a2 und die $a1 sich in der Mitte treffen oder $a2 unter $a1 fällt dann Endet die Funktion
+	lbu $t0, ($t6)			# Laden der Buchstaben in Registern
+	lbu $t1, ($t7)			# s.o.
 
-	seq $a3, $t0, $t1		# If $t1 = $t0 dann wird $a3 auf 1 gesetzt, sonst 0
-	beq $a3, $0, jumpbackPalin	# Jumpback falls false
+	seq $v0, $t0, $t1		# If $t1 = $t0 dann wird $v0 auf 1 gesetzt, sonst 0
+	beq $v0, $0, jumpbackPalin	# Jumpback falls false
 	
-	addiu $a1, $a1, 1		# Erhöhen der Adresse
-	addiu $a2, $a2, -1		# Erniedrigen der Adresse
+	addiu $t6, $t6, 1		# Erhöhen der Adresse
+	addiu $t7, $t7, -1		# Erniedrigen der Adresse
 	j palinCheck
 	
 	
 jumpbackPalin:
-	move $s0, $a3			# In $s0 soll der Rückgabewert gespeichert werden
 	jr $31
 
 # End isPalindrom Function
---
+++ prog7.1.asm
@@ -0,0 +1,87 @@
+.data
+ausgabe1: .asciiz "Geben Sie einen String ein:"
+ausgabe2: .asciiz "Geben Sie einen Char ein:"
+ausgabe3: .asciiz "Moechten Sie das Programm noch einmal laufen lassen?"
+buffer: .space 128
+
+.text
+
+main:
+				# Ausgabe der Aufforderung
+	li $v0, 4		# Code zum Ausgeben eines Strings
+	la $a0, ausgabe1	
+	syscall
+	
+				# Eingabe eines Strings
+	li $v0, 8		# Code zum Eingeben eines Strings
+	la $a0, buffer		# Speicheradresse unseres Strings
+	la $a1, 128		# Max Groeße von dem String (0-127)
+	syscall
+	
+				# Ausgabe der Aufforderung
+	li $v0, 4		
+	la $a0, ausgabe2	
+	syscall
+	
+				# Eingabe zum Lesen eines Char
+	li $v0, 12		# Code zum Einlesen eines Char
+	syscall
+	move $a2, $v0
+	
+	la $a0, buffer
+	jal ncstr
+	move $t0, $v0
+	jal menue
+	
+
+	
+ende:
+	j ende
+	
+menue:	
+				# Ausgabe der Aufforderung
+	li $v0, 4
+	la $a0, ausgabe3	
+	syscall
+				# Eingabe eines Integers wegen der Aufforderung die gestellt wurde
+	li $v0, 50		# Code ConfirmDialog
+	syscall
+	
+	beqz $a0, main		# Falls Die eingabe 0 entspricht springt es zur main
+	jr $31			# Ansonsten springt es zurück 
+	
+	
+	
+# int ncstr(char *str, char c)
+# Start of ncstr
+
+ncstr:
+	addiu $t2, $a0, 0 	# Kopie der Adresse
+	add $v0, $0, $0		# Leeren von $v0
+	
+loopNcstr:
+	lbu $t0, ($t2)		# Laden des Asciizeichen in $t0
+	beqz $t0, jumpbackNcstr	# Falls das Asciizeichen eine 0 ist, wird das Programm beendet
+	addiu $t2, $t2, 1	# Erhöhen der Adresse
+	seq $t1, $t0, $a2	# $t1 wird auf 1 gesetzt, wenn das Asciizeichen in $t0 und der eingegebene Char gleich sind
+	bnez $t1, zaehler	# Falls das Register nicht 0 ist
+	j loopNcstr
+	
+zaehler:
+	addi $v0, $v0, 1	# Zaehler der die Anzahl der gleichen Buchstaben speichert ($s1)
+	j loopNcstr	
+
+jumpbackNcstr:
+	jr $31
+
+
+
+
+
+
+
+
+
+
+
+
